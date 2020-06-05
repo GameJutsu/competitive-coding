99820fb2720c78943cad19d0e80d093f4ddf2420
GameJutsu <GameJutsu@users.noreply.github.com>
2020-06-05 21:46:59 +0530

    Create HS08TEST

---
+++ Practice/Beginner/ATM - HS08TEST/HS08TEST.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	double bal;
+	int wd;
+	cin>>wd;
+	cin>>bal;
+	cout.precision(2);
+	if(wd % 5 == 0 && wd <= (bal - 0.5))
+	{
+	    cout<<fixed<<bal - (wd + 0.5);
+	}
+	else
+	{
+	    
+	    cout<<fixed<<bal;
+	}
+	return 0;
+}

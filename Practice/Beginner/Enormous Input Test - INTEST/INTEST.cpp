65d30bea145607e79a7d7c47ee5ca4b54a5c0fb9
GameJutsu <GameJutsu@users.noreply.github.com>
2020-06-05 21:54:23 +0530

    Create INTEST

---
+++ Practice/Beginner/Enormous Input Test - INTEST/INTEST.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, k, curr, total = 0, i;
+	cin>>n;
+	cin>>k;
+	for(i=0;i<n;i++)
+	{
+	    cin>>curr;
+	    if(curr%k==0)
+	    {
+	        total++;
+	    }
+	}
+	cout<<total;
+	return 0;
+}

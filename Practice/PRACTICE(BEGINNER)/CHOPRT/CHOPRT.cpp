29e73078d5e3c426e719efee6d24849e8f5542e2
GameJutsu <GameJutsu@users.noreply.github.com>
2020-08-30 16:35:55 +0530

    Create CHOPRT

---
+++ Practice/PRACTICE(BEGINNER)/CHOPRT/CHOPRT.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T, A, B;
+	cin>>T;
+	while(T--)
+	{
+	    cin>>A>>B;
+	    if(A>B)
+	    {
+	        cout<<">"<<endl;
+	    }
+	    else if(A<B)
+	    {
+	        cout<<"<"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"="<<endl;
+	    }
+	}
+	return 0;
+}

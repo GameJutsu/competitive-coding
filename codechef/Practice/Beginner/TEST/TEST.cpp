14161758719deecc46b3103c22ce796e7445736d
Kumar Vaibhav <rishavvaibhav@gmail.com>
2021-07-14 00:40:51 +0530

    TEST

---
+++ codechef/Practice/Beginner/TEST/TEST.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a;
+	while(1)
+	{
+	    cin>>a;
+	    if(a != 42)
+	    {
+	        cout<<a<<endl;
+	    }
+	    else
+	    {
+	        return 0;
+	    }
+	}
+}
